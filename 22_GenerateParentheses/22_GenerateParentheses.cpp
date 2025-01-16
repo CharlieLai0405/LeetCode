914c4aa04cafe8c9f27601852521ecad5f6d775d
CharlieLai0405 <charlielai123456789@gmail.com>
2025-01-16 16:42:39 +0800

    Leetcode 22_GenerateParentheses

---
+++ 22_GenerateParentheses/22_GenerateParentheses.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        for (int i = 0; i < n; i++)
+        {
+
+        }
+    }
+};
